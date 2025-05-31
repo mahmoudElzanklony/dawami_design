c2e1a48a642bc2ee7e55fd560a6bcb17531e0504
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2025-06-01 02:42:03 +0300

    debug production axios

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -2,7 +2,8 @@ import {useAuthStore} from "~/stores/Auth/AuthStore.js";
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
     const token = useCookie('authToken')
-
+    console.log('to ===>',to)
+    console.log('from===>',from)
     if (to.path !== '/login' && !token.value) {
         return navigateTo('/login')
     }
